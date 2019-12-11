9e1f388444429638035f6548203d462f5090e7e6
Andreas_Rauscher <andreas@sprachkurse-weltweit.de>
2019-12-11 12:58:09 +0100

    prepared auto updater functions

---
+++ js/update.js
@@ -0,0 +1,66 @@
+const { ipcRenderer } = require('electron')
+const version = document.getElementById('version')
+const updateNode = document.getElementById('update-checker')
+var updateNote
+
+ipcRenderer.send('app_version')
+
+ipcRenderer.on('app_version', (event, arg) => {
+ipcRenderer.removeAllListeners('app_version')
+  version.innerText = `Version: ${arg.version}`
+})
+
+ipcRenderer.on('update_available', () => {
+  ipcRenderer.removeAllListeners('update_available')
+  updateChecker(true)
+})
+
+ipcRenderer.on('update_downloaded', () => {
+  ipcRenderer.removeAllListeners('update_downloaded')
+  updateChecker(false)
+  showUpdateInstallScreen()
+})
+
+// UPDATE CHECK NOTIFICATION
+function updateChecker(state){
+  if(state){
+    updateNode.style.display = 'inline'
+    updateNote = window.setInterval( function() {
+      var dotLoader = document.getElementById("dot-loader")
+      if ( dotLoader.innerHTML.length > 3 ){
+        dotLoader.innerHTML = ""
+      }
+      else {
+        dotLoader.innerHTML += "."
+      }
+    }, 200)
+  }
+  else {
+    updateNode.style.display = 'none'
+    clearInterval(updateNote)
+  }
+}
+
+function showUpdateInstallScreen(){
+  let options = {
+    type: 'info',
+    buttons: [`Installieren`, `Später`],
+    defaultId: 0,
+    title: 'Auto Updater',
+    detail: 'Ein neues Update ist verfügbar.'
+  }
+  dialog.showMessageBox(null, options, (response) => {
+    // abort
+    if(response == 1){
+      return false
+    }
+    // continue
+    if(response == 0){
+      restartApp()
+    }
+  })
+}
+
+function restartApp() {
+  ipcRenderer.send('restart_app');
+}
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const {electron, app, BrowserWindow} = require('electron')
+const {electron, app, BrowserWindow, ipcMain} = require('electron')
+const { autoUpdater } = require('electron-updater')
 const path = require('path')
 
-function createWindow() {
+let win
 
-  let win = new BrowserWindow({
+function createWindow() {
+  win = new BrowserWindow({
     width: 1180,
     height: 820,
     minWidth: 900,
@@ -27,6 +29,23 @@ function createWindow() {
 
 app.on('ready', () => {
   createWindow()
+  autoUpdater.checkForUpdatesAndNotify()
 })
 
 app.on('window-all-closed', () => {app.quit()})
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available')
+})
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded')
+})
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall()
+})
